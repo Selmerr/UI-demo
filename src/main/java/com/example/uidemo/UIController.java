ef0cf33532ba0c7b4a659251e47a761823f6f9b2
Selmerr <115707577+Selmerr@users.noreply.github.com>
2023-02-28 20:52:51 +0100

    Added html and controller to sidebar branch

---
+++ src/main/java/com/example/uidemo/UIController.java
@@ -0,0 +1,13 @@
+package com.example.uidemo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class UIController {
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+}
